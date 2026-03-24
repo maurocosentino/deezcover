84b6bc79f2dacbe4ad33c1f4fa032f938f6660cb
maurocosentino <mauroxcosentino@gmail.com>
2026-03-24 16:26:45 -0300

    feat(di): add RepositoryModule binding SongRepository to SongRepositoryImpl

--- app/src/main/java/com/mauro/offlinefirst/di/RepositoryModule.kt
+++ app/src/main/java/com/mauro/offlinefirst/di/RepositoryModule.kt
@@ -1,1 +1,20 @@
 package com.mauro.offlinefirst.di
+
+import com.mauro.offlinefirst.data.repository.SongRepositoryImpl
+import com.mauro.offlinefirst.domain.repository.SongRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindSongRepository(
+        impl: SongRepositoryImpl
+    ): SongRepository
+}
