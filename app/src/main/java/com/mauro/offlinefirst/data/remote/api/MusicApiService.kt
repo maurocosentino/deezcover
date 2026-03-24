dae4b4be6d6c49307a914345cbd1fc366c51e365
maurocosentino <mauroxcosentino@gmail.com>
2026-03-24 00:04:28 -0300

    feat(remote): add MusicApiService Retrofit interface

--- app/src/main/java/com/mauro/offlinefirst/data/remote/api/MusicApiService.kt
+++ app/src/main/java/com/mauro/offlinefirst/data/remote/api/MusicApiService.kt
@@ -1,5 +1,9 @@
 package com.mauro.offlinefirst.data.remote.api
+import com.mauro.offlinefirst.data.remote.dto.SongDto
+import retrofit2.http.GET
 
 interface MusicApiService {
-
+    @GET("songs")
+    suspend fun getSongs(): List<SongDto>
 }
+
