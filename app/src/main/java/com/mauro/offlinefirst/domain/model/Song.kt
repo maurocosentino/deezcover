ada81ba160c2f181090c3e618f1fbc567d34c08c
maurocosentino <mauroxcosentino@gmail.com>
2026-03-23 21:12:21 -0300

    feat(domain): add Song domain model

--- app/src/main/java/com/mauro/offlinefirst/domain/model/Song.kt
+++ app/src/main/java/com/mauro/offlinefirst/domain/model/Song.kt
@@ -1,2 +1,10 @@
 package com.mauro.offlinefirst.domain.model
 
+data class Song(
+    val id: String,
+    val title: String,
+    val artist: String,
+    val albumArt: String,
+    val durationMs: Long,
+    val isAvailableOffline: Boolean
+)
