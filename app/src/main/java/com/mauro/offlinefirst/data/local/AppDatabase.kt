3db2728b4bb221c1d915ecc366aadd0a0127b93a
maurocosentino <mauroxcosentino@gmail.com>
2026-03-23 22:09:32 -0300

    feat(local): add AppDatabase Room configuration

--- app/src/main/java/com/mauro/offlinefirst/data/local/AppDatabase.kt
+++ app/src/main/java/com/mauro/offlinefirst/data/local/AppDatabase.kt
@@ -1,2 +1,16 @@
 package com.mauro.offlinefirst.data.local
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.mauro.offlinefirst.data.local.dao.SongDao
+import com.mauro.offlinefirst.data.local.entity.SongEntity
+
+@Database(entities =  [SongEntity::class], exportSchema = true, version = 1)
+abstract class AppDatabase : RoomDatabase(){
+
+    abstract fun songDao(): SongDao
+
+    companion object {
+        const val DATABASE_NAME = "offline_first_db"
+    }
+}
