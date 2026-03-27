5efb9bdd71e199867e98842e3f61616356fc1488
maurocosentino <mauroxcosentino@gmail.com>
2026-03-26 21:25:57 -0300

    feat(navigation): integrate NavGraph in MainActivity

--- app/src/main/java/com/mauro/offlinefirst/MainActivity.kt
+++ app/src/main/java/com/mauro/offlinefirst/MainActivity.kt
@@ -4,7 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.mauro.offlinefirst.presentation.songlist.SongListScreen
+import androidx.navigation.compose.rememberNavController
+import com.mauro.offlinefirst.presentation.navigation.NavGraph
 import com.mauro.offlinefirst.ui.theme.AndroidofflinefirstTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,7 +16,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidofflinefirstTheme {
-                SongListScreen()
+                val navController = rememberNavController()
+                NavGraph(navController = navController)
             }
         }
     }
