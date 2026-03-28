acc1b6a97405027917f12b0dec752b70fe2a43b9
maurocosentino <mauroxcosentino@gmail.com>
2026-03-28 03:12:01 -0300

    feat(presentation): add isConnected to SongDetailUiState

--- app/src/main/java/com/mauro/offlinefirst/presentation/songdetail/SongDetailUiState.kt
+++ app/src/main/java/com/mauro/offlinefirst/presentation/songdetail/SongDetailUiState.kt
@@ -4,7 +4,8 @@ import com.mauro.offlinefirst.domain.model.Song
 
 data class SongDetailUiState(
     val song: Song? = null,
-    val playerState: PlayerState = PlayerState.IDLE
+    val playerState: PlayerState = PlayerState.IDLE,
+    val isConnected: Boolean = true
 )
 
 enum class PlayerState {
