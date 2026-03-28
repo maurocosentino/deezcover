647d50691e492787bd159503880b53a178c08047
maurocosentino <mauroxcosentino@gmail.com>
2026-03-28 14:08:23 -0300

    feat(remote): add getAlbumTracks endpoint to MusicApiService

--- app/src/main/java/com/mauro/offlinefirst/data/remote/api/MusicApiService.kt
+++ app/src/main/java/com/mauro/offlinefirst/data/remote/api/MusicApiService.kt
@@ -1,9 +1,15 @@
 package com.mauro.offlinefirst.data.remote.api
 import com.mauro.offlinefirst.data.remote.dto.DeezerChartResponseDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface MusicApiService {
     @GET("chart/0/tracks")
     suspend fun getSongs(): DeezerChartResponseDto
+
+    @GET("album/{albumId}/tracks")
+    suspend fun getAlbumTracks(
+        @Path("albumId") albumId: String
+    ): DeezerChartResponseDto
 }
 
