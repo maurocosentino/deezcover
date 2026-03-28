ef5aef3f11b4e3ebf33b4c923d14ce3eba43ac06
maurocosentino <mauroxcosentino@gmail.com>
2026-03-28 03:11:53 -0300

    feat(local): update AppDatabase version for previewUrl migration

--- app/src/main/java/com/mauro/offlinefirst/data/local/AppDatabase.kt
+++ app/src/main/java/com/mauro/offlinefirst/data/local/AppDatabase.kt
@@ -5,12 +5,12 @@ import androidx.room.RoomDatabase
 import com.mauro.offlinefirst.data.local.dao.SongDao
 import com.mauro.offlinefirst.data.local.entity.SongEntity
 
-@Database(entities =  [SongEntity::class], exportSchema = false, version = 2)
+@Database(entities =  [SongEntity::class], exportSchema = false, version = 3)
 abstract class AppDatabase : RoomDatabase(){
 
     abstract fun songDao(): SongDao
 
     companion object {
-        const val DATABASE_NAME = "offline_first_db_v2"
+        const val DATABASE_NAME = "offline_first_db_v3"
     }
 }
