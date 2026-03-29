6201c75e0182da1a7cb6d88740e2fc065fda82c3
maurocosentino <mauroxcosentino@gmail.com>
2026-03-28 23:55:39 -0300

    feat(remote): add fetchChartAlbums to RemoteDataSource

--- app/src/main/java/com/mauro/offlinefirst/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/mauro/offlinefirst/data/remote/RemoteDataSource.kt
@@ -1,16 +1,19 @@
 package com.mauro.offlinefirst.data.remote
 
 import com.mauro.offlinefirst.data.remote.api.MusicApiService
+import com.mauro.offlinefirst.data.remote.dto.AlbumDto
 import com.mauro.offlinefirst.data.remote.dto.SongDto
 
 class RemoteDataSource constructor(
-   private val apiService : MusicApiService,
-){
+    private val apiService: MusicApiService,
+) {
     suspend fun fetchSongs(): List<SongDto> {
         return apiService.getSongs().tracks
     }
-
     suspend fun fetchAlbumTracks(albumId: String): List<SongDto> {
         return apiService.getAlbumTracks(albumId).tracks
+    }
+    suspend fun fetchChartAlbums(): List<AlbumDto> {
+        return apiService.getChartAlbums().albums
     }
 }
