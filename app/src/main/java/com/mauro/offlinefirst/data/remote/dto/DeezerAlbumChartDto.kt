fc16633d59ba34a4aaaaafad10c874c39ada8fe6
maurocosentino <mauroxcosentino@gmail.com>
2026-03-28 23:55:39 -0300

    feat(remote): add DeezerAlbumChartDto and update DeezerChartResponseDto

--- app/src/main/java/com/mauro/offlinefirst/data/remote/dto/DeezerAlbumChartDto.kt
+++ app/src/main/java/com/mauro/offlinefirst/data/remote/dto/DeezerAlbumChartDto.kt
@@ -1,4 +1,16 @@
 package com.mauro.offlinefirst.data.remote.dto
 
-class DeezerAlbumChartDto {
-}
+import com.google.gson.annotations.SerializedName
+
+data class DeezerAlbumChartDto(
+    @SerializedName("data") val albums: List<AlbumDto>
+)
+data class AlbumDto(
+    @SerializedName("id")          val id: Long,
+    @SerializedName("title")       val title: String,
+    @SerializedName("cover_medium") val coverMedium: String,
+    @SerializedName("artist")      val artist: AlbumArtistDto
+)
+data class AlbumArtistDto(
+    @SerializedName("name") val name: String
+)
--- app/src/main/java/com/mauro/offlinefirst/data/remote/dto/DeezerChartResponseDto.kt
+++ app/src/main/java/com/mauro/offlinefirst/data/remote/dto/DeezerChartResponseDto.kt
@@ -1,8 +1,7 @@
 package com.mauro.offlinefirst.data.remote.dto
 
 import com.google.gson.annotations.SerializedName
-
-data class DeezerChartResponseDto (
+data class DeezerChartResponseDto(
     @SerializedName("data") val tracks: List<SongDto>,
     @SerializedName("total") val total: Int
 )
