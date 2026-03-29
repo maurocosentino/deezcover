0dff0fdcc7ba3d39798a0e32ea10202270de173e
maurocosentino <mauroxcosentino@gmail.com>
2026-03-28 22:01:28 -0300

    feat(dto): add album and preview fields to Deezer DTOs

--- app/src/main/java/com/mauro/offlinefirst/data/remote/dto/DeezerAlbumDto.kt
+++ app/src/main/java/com/mauro/offlinefirst/data/remote/dto/DeezerAlbumDto.kt
@@ -5,5 +5,5 @@ import com.google.gson.annotations.SerializedName
 data class DeezerAlbumDto(
     @SerializedName("cover_medium") val coverMedium: String,
     @SerializedName("title") val albumTitle: String,
-    @SerializedName("id") val albumId: Long
+    @SerializedName("id") val albumId: Long,
 )
--- app/src/main/java/com/mauro/offlinefirst/data/remote/dto/SongDto.kt
+++ app/src/main/java/com/mauro/offlinefirst/data/remote/dto/SongDto.kt
@@ -6,7 +6,7 @@ data class SongDto(
     @SerializedName("id") val id: Long,
     @SerializedName("title") val title: String,
     @SerializedName("artist") val artist: DeezerArtistDto,
-    @SerializedName("album") val albumArt: DeezerAlbumDto,
+    @SerializedName("album") val albumArt: DeezerAlbumDto?,
     @SerializedName("duration") val duration: Long,
     @SerializedName("link") val link: String,
     @SerializedName("preview") val previewUrl: String
